eb322000226cf31abe6fa87e1b7f3bf2d63857fe
Martin J. Lenardon <martin.lenardon@L2Analytics.com>
2016-01-10 20:59:54 -0500

    Initial ProjectScaffold commit

---
+++ RELEASE_NOTES.md
@@ -0,0 +1,2 @@
+#### 0.0.1 January 10 2016
+* Initial commit
---
+++ src/Finance/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Finance")>]
+[<assembly: AssemblyProductAttribute("Finance")>]
+[<assembly: AssemblyDescriptionAttribute("Tools for Financial calculations")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
